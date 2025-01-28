7057c843e17e3df320f6b4596a5e6844f31a2056
aadippel <aadippel@gmail.com>
2025-01-28 19:39:27 +0000

    corrected code to fix QR code use

--- qr.rb
+++ qr.rb
@@ -1,11 +1,6 @@
 # Write your solution here!
 require "rqrcode"
 
-user_input = gets.chomp
-
-puts user_input
-puts “What kind of QR code would you like to generate? (1 for URL, 2 for wifi network, 3 for text message, 4 to exit)”
-
 loop do
   puts "What kind of QR code would you like to generate?"
   puts "1. URL"
@@ -16,37 +11,48 @@ loop do
   user_input = gets.chomp
 
   if user_input == "1"
-    url_input = gets.chomp
-    puts url_input
     puts "What is the URL?"
-    qrcode = RQRCode::QRCode.new(url_input)
+    url_input = gets.chomp
+    puts "Encoding URL: #{url_input}" # This will confirm what URL is being encoded.
+    
+    qrcode = RQRCode::QRCode.new(url_input) # Generate and save QR code for URL
+    png = qrcode.as_png({ :size => 500 })
+    file_name = "url_qr_code.png"
+    IO.binwrite(file_name, png.to_s)
+   
+    puts "QR code generated and saved as '#{file_name}'."
+
   elsif user_input == "2"
+    puts "What is the WiFi network name?"
     wifi_network_input = gets.chomp
-    puts wifi_network_input
-    puts "What is the WiFi network?"
-    wifi_password_input = gets.chomp
-    puts wifi_password_input
     puts "What is the WiFi password?"
-    qrcode = WIFI:T:WPA;S:wifi_network_input;P:wifi_password_input;;
+    wifi_password_input = gets.chomp
+    encoded_wifi_details = "WIFI:T:WPA;S:#{wifi_network_input};P:#{wifi_password_input};;"
+   
+    qrcode = RQRCode::QRCode.new(encoded_wifi_details) # Generate and save QR code for WiFi
+    png = qrcode.as_png({ :size => 500 })
+    file_name = "url_qr_code.png"
+    IO.binwrite(file_name, png.to_s)
+   
+    puts "QR code generated and saved as '#{file_name}'."
+
   elsif user_input == "3"
-    mobile_number_input = gets.chomp
-    puts mobile_number_input
     puts "What is the mobile number? (Please give only numbers and no spaces)"
-    SMSTO:mobile_number_input:Hi, it's
-    break # Remove this line after implementing the QR code generation
+    mobile_number_input = gets.chomp
+    puts "What is the message you want to send?"
+    message_input = gets.chomp
+    encoded_sms_details = "SMSTO:#{mobile_number_input}:#{message_input}"
+
+    qrcode = RQRCode::QRCode.new(encoded_sms_details) # Generate and save QR code for SMS
+    png = qrcode.as_png({ :size => 500 })
+    file_name = "url_qr_code.png"
+    IO.binwrite(file_name, png.to_s)
+   
+    puts "QR code generated and saved as '#{file_name}'."
   elsif user_input == "4"
     puts "Goodbye!"
     break
   else
-    puts "Invalid option, please try again."
+    puts "Invalid option, please try choose 1 through 4."
   end
 end
-
-# Use the RQRCode::QRCode class to encode some text
-qrcode = SMSTO:"9876543210":"Hi Alice! It's"
-
-# Use the .as_png method to create a 500 pixels by 500 pixels image
-png = qrcode.as_png({ :size => 500 })
-
-# Write the image data to a file
-IO.binwrite("sometext.png", png.to_s)
